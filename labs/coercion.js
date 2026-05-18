403be56592e6b9c830af5b75e73af4908f74ecf3
mwanasitiali <alimwanasiti94@gmail.com>
2026-05-18 14:48:56 +0300

    feat: created a debug type coercion error

---
+++ labs/coercion.js
@@ -0,0 +1,18 @@
+
+const firstResult = 15 -"0";
+console.log(`15 - 0 = ${firstResult}`);
+
+const secondResult = "8" - 5;
+console.log(`8 - 5 = ${secondResult}`);
+
+const thirdResult = 1 + 5;
+console.log(`1 + 5 = ${thirdResult}`);
+
+const fourthResult = 7 + 1;
+console.log(`7 + 1 = ${fourthResult}`);
+
+const fifthResult = 10 * 2;
+console.log(`10 * 2 = ${fifthResult}`);
+
+const sixthResult = 20 + 2;
+console.log(`20 + 2 = ${sixthResult}`);
