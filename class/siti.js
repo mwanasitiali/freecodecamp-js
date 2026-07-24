0ba05330d8cffd2ca13faa9798013bdd1d8fb58c
mwanasitiali <alimwanasiti94@gmail.com>
2026-07-24 17:07:11 +0300

    feat: created space mission roster

---
+++ class/siti.js
@@ -0,0 +1,4 @@
+console.log(siti);
+let number = [1,2,3,4,5];
+number.push(6);
+console.log(number.push);
---
+++ exercise_4.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3];
+console.log(numbers[10]);
+
+
+
+const developers = ["Jessicca", "Naomi", "Siti"];
+developers[0];
+
+console.log("Jessica");
+
+
+const numberz = [10, 20, 30, 40];
+const index = numbers.indexOf(20);
+console.log(index);
+
