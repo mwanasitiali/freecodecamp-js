e07ea815cf949c7ca5fd19fd9699a169339f91a3
mwanasitiali <alimwanasiti94@gmail.com>
2026-08-12 12:32:17 +0300

    feat: created number sum calculator

---
+++ labs/sum_calculator.js
@@ -0,0 +1,26 @@
+function sumPrimes(n) {
+  if (n < 2) {
+    return 0;
+  }
+
+  function isPrime(num) {
+    if (num < 2) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let sum = 0;
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
